6b74373e855486106bb68774392f98670ebe433d
nn <nn@eworx.gr>
2014-09-15 12:22:22 +0200

    Set currentCallbackId on encoding start

--- src/ios/WavToAacConverter.m
+++ src/ios/WavToAacConverter.m
@@ -24,6 +24,7 @@
     
     NSString* sourcePath = (NSString*)[command.arguments objectAtIndex:0];
     NSString* destinationPath = (NSString*)[command.arguments objectAtIndex:1];
+    _currentCommandCallbackId = command.callbackId;
     _audioConverter = [[TPAACAudioConverter alloc] initWithDelegate:self source:sourcePath destination:destinationPath];
     [_audioConverter start];
 }
